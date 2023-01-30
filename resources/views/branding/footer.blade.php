a4cc9018741b27bbcdaffdfad33ee4660c32ec2d
Egor Repiev <egor.repiev@gmail.com>
2023-01-30 14:39:47 +0500

    Поправили брендинг.

--- resources/views/branding/footer.blade.php
+++ resources/views/branding/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="row text-center">
   <span class="col-12 small m-n">
-    ГАПОУ СО "Нижнетагильский торгово-экономический колледж" © {{date('Y')}} 
+    ERP<span style="font-size: .575rem;">&</span>ID © ГАПОУ СО "Нижнетагильский торгово-экономический колледж" {{date('Y')}} 
   </span>
 </div>
--- resources/views/branding/header.blade.php
+++ resources/views/branding/header.blade.php
@@ -9,7 +9,7 @@
 <p class="h2 n-m font-thin v-center">
   <x-orchid-icon path="cloud-upload" />
   <span class="m-l d-none d-sm-block ms-2">
-    ERP
+    ERP<small>&</small>ID
     <small class="v-top opacity">НТТЭК</small>
   </span>
 </p>
