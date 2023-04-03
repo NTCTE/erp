8414bee47aa8503017aea42fb087937f91871919
Egor Repiev <egor.repiev@gmail.com>
2023-04-03 09:22:45 +0500

    Добавлено удаление команды.

--- app/Orchid/Layouts/System/Machines/Tables/CommandsTable.php
+++ app/Orchid/Layouts/System/Machines/Tables/CommandsTable.php
@@ -3,6 +3,10 @@
 namespace App\Orchid\Layouts\System\Machines\Tables;
 
 use App\Models\System\Machines\Command;
+use Orchid\Screen\Actions\Button;
+use Orchid\Screen\Actions\DropDown;
+use Orchid\Screen\Actions\Link;
+use Orchid\Screen\Actions\ModalToggle;
 use Orchid\Screen\Fields\Code;
 use Orchid\Screen\Layouts\Table;
 use Orchid\Screen\TD;
@@ -38,6 +42,20 @@ class CommandsTable extends Table
                 -> width('10%'),
             TD::make('created_at', 'Дата создания')
                 -> width('15%'),
+            TD::make('actions', 'Действия')
+                -> render(function (Command $command) {
+                    return DropDown::make()
+                        -> icon('options-vertical')
+                        -> list([
+                            Button::make('Удалить команду')
+                                -> icon('trash')
+                                -> method('remove')
+                                -> confirm('Вы уверены, что хотите удалить команду?')
+                                -> parameters([
+                                    'id' => $command -> id,
+                                ]),
+                        ]);
+                }),
         ];
     }
 }
