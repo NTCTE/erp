847cfff55d86f797690d79bbec621787a7094c4a
Sharindog <46196147+sharindog@users.noreply.github.com>
2023-06-20 14:18:07 +0500

    1. ? Upload Error Fix

--- app/Orchid/Screens/Library/EditBooksetScreen.php
+++ app/Orchid/Screens/Library/EditBooksetScreen.php
@@ -151,7 +151,7 @@ class EditBooksetScreen extends Screen
                     ->targetId(),
                 Upload::make('bookset.digitized_id')
                     ->title('Цифровизированный экземпляр')
-                    ->maxFileSize(128)
+                    ->maxFileSize(40)
                     ->acceptedFiles('.pdf')
                     ->storage('public')
                     ->maxFiles(1)
